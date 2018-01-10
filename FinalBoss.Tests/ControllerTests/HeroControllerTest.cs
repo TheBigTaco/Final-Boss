16b527d256ad264dfd44394acf0eb02fcd432dac
Adam Titus and Kimberly Bordon <adam+kimberly@>
2018-01-10 14:05:59 -0800

    freak out at the beach ball of doom

---
+++ FinalBoss.Tests/ControllerTests/HeroControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinalBoss.Models;
+using FinalBoss.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace FinalBoss.Tests.ControllerTests
+{
+    [TestClass]
+    public class HeroControllerTest
+    {
+        Mock<IHeroRepository> mock = new Mock<IHeroRepository>();
+        EFHeroRepository db = new EFHeroRepository(new TestDbContext());
+
+        public void Dispose()
+        {
+            db.DeleteAll();
+        }
+
+        private void DbSetup()
+        {
+            mock.Setup(e => e.Heroes).Returns()
+        }
+    }
+}
